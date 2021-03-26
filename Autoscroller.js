08460c646b2a404cf8873d6e86de19c6d0206863
Avinash <avint8@gmail.com>
2021-03-26 11:48:24 +0530

    Update Autoscroller.js

--- Autoscroller.js
+++ Autoscroller.js
@@ -2,15 +2,31 @@ var speed = 2;
 var Sheight;
 var c = 0;
 var caller;
+var f = 1;
+
+document.body.onclick = clicking;
+
+function callerr (){ 
+    caller = setInterval(scrolling,speed);
+}
 
 function scrolling(){
     Sheight = document.body.scrollHeight;
     c = c + 1;
-    console.log(c,Sheight);
     if(c == Sheight){
         clearInterval(caller);
     }
     scrollTo(0,c);
 }
 
-caller = setInterval(scrolling,speed);
+function clicking(){
+    console.log("cscscscsc");
+    if(f == 0){
+        clearInterval(caller);
+        f = 1;
+    }
+    else{
+        callerr();
+        f = 0;
+    }
+}
